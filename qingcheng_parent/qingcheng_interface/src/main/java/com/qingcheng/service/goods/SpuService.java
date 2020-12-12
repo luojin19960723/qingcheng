5296705a8734a6441ed2ace1404f8c47e3cdb95d
Your Name <you@example.com>
2020-12-13 00:22:25 +0800

    前四天的作业

---
+++ qingcheng_parent/qingcheng_interface/src/main/java/com/qingcheng/service/goods/SpuService.java
@@ -0,0 +1,45 @@
+package com.qingcheng.service.goods;
+import com.qingcheng.entity.PageResult;
+import com.qingcheng.pojo.goods.Goods;
+import com.qingcheng.pojo.goods.Spu;
+
+import java.util.*;
+
+/**
+ * spu业务逻辑层
+ */
+public interface SpuService {
+
+    //保存商品
+    public void saveGoods(Goods goods);
+
+
+    public Goods findGoodsById(String id);
+
+    //商品审核
+    public void audit(String id,String status,String message);
+
+
+    public List<Spu> findAll();
+
+
+    public PageResult<Spu> findPage(int page, int size);
+
+
+    public List<Spu> findList(Map<String,Object> searchMap);
+
+
+    public PageResult<Spu> findPage(Map<String,Object> searchMap,int page, int size);
+
+
+    public Spu findById(String id);
+
+    public void add(Spu spu);
+
+
+    public void update(Spu spu);
+
+
+    public void delete(String id);
+
+}
